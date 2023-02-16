7163ab542e4cf41b8f75d2f50a91a7e784b91b03
johannpa <johann_paque@hotmail.fr>
2023-02-16 20:59:00 +0100

    modifications on app.js and header.js, add login component and login css issues with firebase

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          
           <Route path="/checkout">
             <Header />
             <Checkout />
